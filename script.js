746deb7f819e857f4c00608febdae8a06eec16b1
Gilberto_Moura <gylberto120.gm@gmail.com>
2024-05-26 17:31:36 -0300

    comita ultima atualizacao

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    // Ao clicar no botão, revela ou oculta o conteúdo adicional
+    $('#botao-mostrar-mais').click(function(){
+        $('#conteudo-adicional').slideToggle();
+    });
+});
